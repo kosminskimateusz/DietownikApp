7988510dbea50ce7f7c4d01df40ba521a03a2926
Mateusz Kośmiński <kosminskimateusz@gmail.com>
2021-12-15 13:24:51 +0100

    Next step

---
+++ DietownikApp/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace DietownikApp.Models
+{
+    public class Root
+    {
+        public Product[] data { get; set; }
+        public object error { get; set; }
+    }
+
+    public class Product
+    {
+        public string name { get; set; }
+        public float kcal { get; set; }
+        public float fats { get; set; }
+        public float carbs { get; set; }
+        public float proteins { get; set; }
+        public int id { get; set; }
+    }
+
+
+}
